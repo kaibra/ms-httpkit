554e5b8250da078710a8a57a05d4a312d065e17a
Kai Brandes <kai.brandes@otto.de>
2016-02-27 21:20:38 +0100

    making mount-ms a provided dependency

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject kaibra/ms-httpkit "0.0.1"
+(defproject kaibra/ms-httpkit "0.0.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [http-kit "2.1.19"]
-                 [kaibra/mount-ms "0.0.1"]])
+                 [http-kit "2.1.19"]]
+  :profiles {:dev {:dependencies [[kaibra/mount-ms "0.0.2"]]}}
+  )
